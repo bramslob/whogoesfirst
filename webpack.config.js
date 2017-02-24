8b197527626e22a613f4ef1f30c58c933c4478b3
Bram Slob <b.slob@solvari.com>
2017-02-24 15:03:02 +0100

    Initial setup of webpack, sass and JS

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path                = require("path");
+var webpack             = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = [
+    {
+        name   : 'wgf-jQuery',
+        entry  : [
+            './jQuery/src/js/app.js',
+            './jQuery/src/sass/style.scss'
+        ],
+        output : {
+            path    : path.join(__dirname, '/jQuery/dist'),
+            filename: 'js/app.js'
+        },
+        module : {
+            rules: [{
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            }]
+        },
+        plugins: [
+            new ExtractTextPlugin("css/styles.css")
+        ]
+    }
+];
